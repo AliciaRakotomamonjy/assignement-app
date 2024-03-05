8ce58a940e0d2cd7f42e2ff6fd5e688803203cb5
HenintsoaRatsim <ratsimhenintsoa@gmail.com>
2024-03-05 10:21:34 +0300

    commit environnement

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+
+  production: true,
+
+  API_URL: 'http://localhost:3000',
+
+  UTILISATEUR_API: '/api/utilisateur',
+  UTILISATEUR: {
+    LOGIN:'/login',
+    INSCRIPTION:'/inscription'
+  },
+};
