626a7751ffdcc9d162c1d3d088c271c9cb0607cf
HenintsoaRatsim <ratsimhenintsoa@gmail.com>
2024-04-10 13:53:49 +0300

    commit

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
-// Définition du dossier de build de l'application Angular
 const angularBuildPath = path.join(__dirname, 'dist/assignement-app');
-
-// Middleware pour servir les fichiers statiques de l'application Angular
 app.use(express.static(angularBuildPath));
-
-// Route pour toutes les URL, renvoie l'index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(angularBuildPath, 'index.html'));
 });
-
-// Port d'écoute du serveur
 const port = process.env.PORT || 3000;
-
-// Démarrage du serveur
 app.listen(port, () => {
   console.log(`Serveur démarré sur le port ${port}`);
 });
