5d0d11f041d8e3862d5888c6f64953c375799ba8
AliciaRakotomamonjy <aliciarakotomamonjy@gmail.com>
2024-05-24 08:35:50 +0300

    debug mes devoir

--- src/app/shared/Services/assignment-eleve.service.ts
+++ src/app/shared/Services/assignment-eleve.service.ts
@@ -12,31 +12,36 @@ export class AssignmentEleveService {
   constructor(private httpClient: HttpClient) { }
 
   GetAssignmentEleveById(id : string): Observable<AssignmentElve> {
-    let url = environment.API_URL+environment.ASSIGNMENT_API+environment.ASSIGNMENT.DETAIL_ASSIGNEMENT_ELEVE+id;
+    let url = environment.API_URL+environment.ASSIGNMENTELEVE_API+id;
     return this.httpClient.get<AssignmentElve>(url)
   }
 
   EditeAssignment(assignment:AssignmentElve |undefined):Observable<any>{
-    let url = environment.API_URL + environment.ASSIGNMENT_API + environment.ASSIGNMENT.AJOUTER_NOTE_ASSIGNMENT_ELEVE;
+    let url = environment.API_URL + environment.ASSIGNMENTELEVE_API+assignment?._id+"/notes";
     return this.httpClient.put<AssignmentElve>(url, assignment);
   }
 
   GetAllAssignmentEleveWithPagination(page: number, limit : number, filtre: any): Observable<any>{
-    let url = environment.API_URL + environment.ASSIGNMENT_API + environment.ASSIGNMENT.GET_ASSIGNMENT_ELEVE;
+    let url = environment.API_URL + environment.ASSIGNMENTELEVE_API;
     url += `?page=${page}`;
     url += `&limit=${limit}`;
     if (filtre) {
       if (filtre.idMatiere) {
-        url += `&idMatiere=${filtre.idMatiere}`;
+        url += `&idMatiere=${filtre?.idMatiere}`;
       }
       if (filtre.dateDebut) {
-        url += `&dateDebut=${filtre.dateDebut}`;
+        url += `&dateDebut=${filtre?.dateDebut?.toISOString()?.split('T')[0]}`;
       }
       if (filtre.dateFin) {
-        url += `&dateFin=${filtre.dateFin}`;
+        url += `&dateFin=${filtre?.dateFin?.toISOString()?.split('T')[0]}`;
       }
     }
     
     return this.httpClient.get<any>(url)
+  }
+
+  TelechargerFichierEleve(assignmentEleve: AssignmentElve){
+    let url = environment.API_URL + environment.ASSIGNMENTELEVE_API+assignmentEleve?._id+"/fichier";
+    return this.httpClient.get(url, { responseType: 'blob' });
   }
 }
