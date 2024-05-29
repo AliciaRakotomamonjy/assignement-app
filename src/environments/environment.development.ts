4a95096f3f12f2abe83d03b2c4e108a7790f094c
HenintsoaRatsim <ratsimhenintsoa@gmail.com>
2024-05-29 16:53:13 +0300

    commit url en ligne

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
 
   production: false,
 
-  API_URL: 'http://localhost:3001',
+  API_URL: 'https://mbds-assignement-api.onrender.com',
 
   AUTH_API: '/api/auth',
   IMAGE_API:'/api/img',
